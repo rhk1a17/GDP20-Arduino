1bb0156cd6f60a0c32614ec7e96d3b2d95782946
rhk1a17 <rhk1a17@soton.ac.uk>
2022-05-20 10:59:45 +0100

    added commets

--- MAIN_CODE_UE4_ACCEL_MAG/MAIN_CODE_UE4_ACCEL_MAG.ino
+++ MAIN_CODE_UE4_ACCEL_MAG/MAIN_CODE_UE4_ACCEL_MAG.ino
@@ -1,23 +1,31 @@
 //======================================Constants===================================
+// Steer and tilt weightage to tune responsiveness of each movement
+// increasing values will reflect directly on UE4
 float steer_weightage = 0.9;
 float tilt_weightage = 0.5;
+// global variable setup 
+String long_str,rotation_val;
+long rpmval;
 //======================================Constants===================================
 
-//======================================Accel===================================
+//========================================Accel=====================================
+// library and constants setup for accelerometer
 #include "BMI088.h"
 float ax = 0, ay = 0, az = 0;
 float gx = 0, gy = 0, gz = 0;
 float mapped_ax;
 int16_t temp = 0;
-//======================================Accel===================================
+//========================================Accel=====================================
 
-//======================================magneto===================================
+//=======================================magneto====================================
+// library and constant setup for magnetomter
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_LSM9DS1.h>
 #include <Adafruit_Sensor.h>  // not used in this demo but required!
-Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
+Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(); // magnetometer object declaration
 
+//magnetometer i2c addresses declaration
 #define LSM9DS1_SCK A5
 #define LSM9DS1_MISO 12
 #define LSM9DS1_MOSI A4
@@ -30,15 +38,12 @@ Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
 float mx, my, mz, heading, bench_heading, mapped_heading;
 //======================================magneto===================================
 
-String long_str,rotation_val;
-long rpmval;
-
-//==========================RPM & fan constants==================================
-const byte RevSensePin = 2;
-const float WheelRadiusInMeters = 0.33;
-const float SFWRadiusInMeteres = 0.05;
-const unsigned long DisplayIntervalMillis = 1;  // Update once per second
-const unsigned long MaxRevTimeMicros = 500000UL; // >0.5 seconds per revolution counts as 0 RPM
+//===========================RPM & fan constants==================================
+const byte RevSensePin = 2; // digital pin 2 on arduino setup. (change according to connection)
+const float WheelRadiusInMeters = 0.33; //Wheel radius
+const float SFWRadiusInMeteres = 0.05; // Sub flywheel radius
+const unsigned long DisplayIntervalMillis = 1;  // Update once per milliseconds
+const unsigned long MaxRevTimeMicros = 500000UL; // >0.5 seconds per revolution counts as 0 RPM (reduce for faster stop reaction)
 
 // Variables used in the ISR and in the main code must be 'volatile'
 volatile unsigned long RevSenseTimeMicros = 0;  //  Time that the rising edge was sensed
@@ -50,18 +55,19 @@ const float WheelCircumferenceInMeters = TWO_PI * WheelRadiusInMeters;
 const float GearRatio = WheelRadiusInMeters/SFWRadiusInMeteres;
 const float SFWCircuferenceInMeters = TWO_PI * SFWRadiusInMeteres;
 
-int max_rpm=300;
-int fan_pin=9;
-float value;
-//==========================RPM & fan constants==================================
+int max_rpm=300; // fan max rpm
+int fan_pin=9; // fan pwm pin
+float value; // constant
+//===========================RPM & fan constants==================================
 
 void setup(void) {
-  Serial.begin(115200);
+  Serial.begin(115200); // baud rate (must be matched to UE4)
   Wire.begin();
   Serial.setTimeout(5);
-  //=======================Accel==========================
+  //=================================Accel====================================
   while(1)
   {
+    // setup connection with accelerometer
     if(bmi088.isConnection())
     {
       bmi088.initialize();
@@ -69,50 +75,50 @@ void setup(void) {
     }
     delay(100);
   }
-  //=======================Accel==========================
-  //=======================Megneto==========================
-  // Try to initialise and warn if we couldn't detect the chip
+  //=================================Accel====================================
+  //================================Megneto===================================
+  // Try to initialise and warn if we couldn't detect the magnetometer
   if (!lsm.begin())
   {
     Serial.println("No Magnetometer");
     while (1);
   }
-  // helper to just set the default scaling we want, see above!
+  // helper to just set the default scaling we want, see declaration at end of code
+  // includes hard offset and obtaining datum at each startup
   setupSensor();
-  //=======================Magneto==========================
-  //=====================RPM & fan Setup========================
-  pinMode(RevSensePin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(RevSensePin), RevSenseISR, RISING);
+  //================================Megneto===================================
+  //============================RPM & fan Setup===============================
+  pinMode(RevSensePin, INPUT); //RPM pin setup
+  attachInterrupt(digitalPinToInterrupt(RevSensePin), RevSenseISR, RISING); //setup interrupt function for RPM
 
-  pinMode(fan_pin,OUTPUT);
-  analogWrite(fan_pin,0);
-  //=====================RPM & fan Setup========================
+  pinMode(fan_pin,OUTPUT); // fan pin setup
+  analogWrite(fan_pin,0); // set fan to 0
+  //============================RPM & fan Setup===============================
 }
 
 void loop() {
     //======================================Accel===================================
-    bmi088.getAcceleration(&ax, &ay, &az);
-    mapped_ax = map(ax,-1000,1000,-90.0,90.0);
+    bmi088.getAcceleration(&ax, &ay, &az); // get acceleration value, ranged from -1000 to +1000 in all 3 axes
+    mapped_ax = map(ax,-1000,1000,-90.0,90.0); // mapping acceleration in x to +90 and -90 degrees
     //======================================Accel===================================
  
     //=====================================Magneto==================================
-    lsm.read();  /* ask it to read in the data */ 
-    /* Get a new sensor event */ 
-    sensors_event_t a, m, g, temp;
+    lsm.read();  // read in IMU data, including all 3 acceleromter, magnetometer, and gyro data
+    sensors_event_t a, m, g, temp; // call event to obtain data
 
-    lsm.getEvent(&a, &m, &g, &temp);
+    lsm.getEvent(&a, &m, &g, &temp); //get data
 
-    mx = m.magnetic.x;
+    // raw magnetic field in all 3 axes
+    mx = m.magnetic.x; 
     my = m.magnetic.y;
     mz = m.magnetic.z;
 
-    // calibration, offset 
+    // calibration, offset (hard offset calibrated)
     mx = mx - (-0.83);
     my = my - (23.61);
     mz = mz - (-10.35);
 
-    //calculate heading
-
+    //calculate current heading
     if (my == 0) {
       heading = (mx < 0) ? PI : 0;
     }
@@ -134,17 +140,17 @@ void loop() {
 
     //convert values in degree
     heading *= 180.0 / PI;
-    heading = (heading - bench_heading)*-1;
-    mapped_heading = map(heading*100,-9000,9000,-100,100);
-
+    heading = (heading - bench_heading)*-1; // calculate heading relative to datum
+    //mapping heading from +90 and -90 degree to +-100, with basic filter
+    mapped_heading = map(heading*100,-9000,9000,-100,100); 
     //scaling calibrated heading to -100 to 100 with -90deg to 90deg
     //=====================================Magneto==================================
 
-    //======================================RPM & fan=====================================
-    static unsigned previousRPM;
+    //====================================RPM & fan=================================
+    static unsigned previousRPM; //previous rpm to check if its the same
 
     // Only update the display once per DisplayIntervalMillis
-    unsigned long currentMillis = millis();
+    unsigned long currentMillis = millis(); 
     static unsigned long previousMillis = 0;
     if (currentMillis - previousMillis >= DisplayIntervalMillis)
     {
@@ -168,32 +174,32 @@ void loop() {
         rpmval = WheelRPM;
     }
 
-    value=map(rpmval,0,max_rpm,0,255);
-    analogWrite(fan_pin,value);
-    //======================================RPM & fan=====================================
+    value=map(rpmval,0,max_rpm,0,255); // fan speed value
+    analogWrite(fan_pin,value); // write fan speed value
+    //====================================RPM & fan=================================
     
     //======================================Rotation Val=====================================
-    // divide total val * weightage (0.9 steer 0.1 tilt) 
-    // -0.45 and -0.05 for offset in UE4
+    // rotation value sent to UE4
     rotation_val = String(((mapped_heading/90.0)*steer_weightage)+(((mapped_ax/90.0)*tilt_weightage))); 
     //======================================Rotation Val=====================================
 
-    //======================================UE4=====================================
-    if (!Serial.available()) return;
+    //===========================================UE4=========================================
+    if (!Serial.available()) return; //check if UE4 is connected
 
-    String str = Serial.readString();
+    String str = Serial.readString(); // read string from UE4 block
     
-    if (str == "data")
+    if (str == "data") // string read from UE4 must be identical to this
     {
         // tilt accel val + steer magneto val + rpm separated by comma
-        long_str = String(rotation_val) + "," + rpmval; 
-        Serial.println(long_str);
+        // any additional data can be added to this line to be sent to UE4 with comma (must be a string)
+        long_str = String(rotation_val) + "," + rpmval; //concatenate
+        Serial.println(long_str); // write to UE4
     }
-    Serial.flush();
-    //======================================UE4=====================================
+    Serial.flush(); // flush port
+    //===========================================UE4=========================================
 }
 
-//===================================RPM Interrupt function=================
+//=====================================RPM Interrupt function================================
 void RevSenseISR()
 {
   static unsigned long revSensePreviousMicros = 0;  // 'static' to retain value between calls
@@ -202,27 +208,18 @@ void RevSenseISR()
   RevTimeMicros = RevSenseTimeMicros - revSensePreviousMicros; // Time for last revolution
   revSensePreviousMicros = RevSenseTimeMicros;
 }
-//===================================RPM Interrupt function=================
+//=====================================RPM Interrupt function================================
 
-//===================================Magnetometer Setup=================
+//========================================Magnetometer Setup=================================
 void setupSensor()
 {
   // 1.) Set the accelerometer range
   lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
-  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_4G);
-  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G);
-  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G);
   // 2.) Set the magnetometer sensitivity
   lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
-  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_8GAUSS);
-  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_12GAUSS);
-  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_16GAUSS);
   // 3.) Setup the gyroscope
   lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
-  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_500DPS);
-  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
-  lsm.read();  /* ask it to read in the data */ 
-  /* Get a new sensor event */ 
+  lsm.read();  //same as above
   sensors_event_t a, m, g, temp;
 
   lsm.getEvent(&a, &m, &g, &temp);
@@ -259,6 +256,6 @@ void setupSensor()
 
   //convert values in degree
   heading *= 180.0 / PI;
-  bench_heading = heading;
+  bench_heading = heading; // setup datum heading
 }
-//===================================Magnetometer Setup=================
+//===================================Magnetometer Setup==============================
